744651ca11a42e289e15bfdfd083981509f8fea9
Evan Weaver <evan@cloudbur.st>
2008-04-25 08:18:08 +0000

    Fix it so it still uses the cache.

--- lib/interlock/finders.rb
+++ lib/interlock/finders.rb
@@ -8,9 +8,9 @@ module Interlock
     # Any other options besides ids short-circuit the cache.
     #    
     def find(*args)
-      args.pop if args.last.is_a? Hash and !args.last.values.any?
-      return find_via_db(*args) if args.last.is_a? Hash or args.last.nil? or args.first.is_a? Symbol
-      
+      args.pop if args.last.nil? or (args.last.is_a? Hash and !args.last.values.any?)
+      return find_via_db(*args) if args.last.is_a? Hash or args.first.is_a? Symbol
+            
       records = find_via_cache(args.flatten, true)
 
       if args.length > 1 or args.first.is_a? Array
