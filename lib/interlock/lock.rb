73aff504a95a4ae17b691771c4820f8ffbdb581c
stopdropandrew <andrew@andrew.local>
2008-10-13 10:51:19 -0700

    Rescue all memcached errors, if the memcached servers are down we should just fall back to the db, not hose the site

--- lib/interlock/lock.rb
+++ lib/interlock/lock.rb
@@ -20,6 +20,9 @@ module Interlock
           # Nil is a successful response for Memcached, so we'll simulate the MemCache
           # API.
           response ||= "STORED\r\n"
+        rescue Memcached::Errors
+          # if memcached raises one of these errors, lets assume the servers down
+          return nil
         rescue Object => e
           # Catch exceptions from Memcached without setting response.
         end
